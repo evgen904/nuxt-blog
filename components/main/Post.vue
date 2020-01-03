dd2ddb15d37058fb8e8968b3d6aae74a3416d524
Evgen904 <evgen904@gmail.com>
2020-01-03 19:11:10 +0300

    Главная страница

---
+++ components/main/Post.vue
@@ -0,0 +1,19 @@
+<template>
+  <article class="post">
+
+  </article>
+</template>
+
+<script>
+export default {
+  name: "Post"
+}
+</script>
+
+<style lang="scss" scope>
+  .post {
+    height: 200px;
+    border: 1px solid #000;
+    margin-bottom: 2rem;
+  }
+</style>
