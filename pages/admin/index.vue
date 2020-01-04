a5f8fc7ac95a431c3b4f441ccfc5bd8141ddc0f4
Evgen904 <evgen904@gmail.com>
2020-01-04 19:10:04 +0300

    Панель администратора, Создание структуры страниц.

---
+++ pages/admin/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>dsf sdfsdf</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page admin',
+  // в какой layout будет направлен роут
+  layout: 'admin'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
