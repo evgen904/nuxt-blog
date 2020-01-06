f262b8d8725c4b4863a77e7c631fe769e84f09a5
Evgen904 <evgen904@gmail.com>
2020-01-07 02:41:26 +0300

    Список постов. Часть 2

--- pages/admin/list.vue
+++ pages/admin/list.vue
@@ -32,18 +32,23 @@
     <el-table-column
       label="Действия">
       <template slot-scope="{row}">
-        <el-button
-          icon="el-icon-edit"
-          type="primary"
-          circle
-          @click="open(row._id)"
-        />
-        <el-button
-          icon="el-icon-delete"
-          type="danger"
-          circle
-          @click="remove(row._id)"
-        />
+
+        <el-tooltip effect="dark" content="Открыть пост" placement="top">
+          <el-button
+            icon="el-icon-edit"
+            type="primary"
+            circle
+            @click="open(row._id)"
+          />
+        </el-tooltip>
+        <el-tooltip effect="dark" content="Удалить пост" placement="top">
+          <el-button
+            icon="el-icon-delete"
+            type="danger"
+            circle
+            @click="remove(row._id)"
+          />
+        </el-tooltip>
       </template>
     </el-table-column>
   </el-table>
@@ -55,15 +60,25 @@ export default {
   layout: 'admin',
   middleware: ['admin-auth'],
   async asyncData({store}) {
-    const posts = await store.dispatch('posts/fetchAdmin')
+    const posts = await store.dispatch('post/fetchAdmin')
     return {posts}
   },
   methods: {
     open(id) {
-      console.log('open', id);
+      this.$router.push(`/admin/post/${id}`)
     },
-    remove(id) {
-      console.log('remove', id);
+    async remove(id) {
+      try {
+        await this.$confirm('Удалить пост ?', 'Внимание!', {
+          confirmButtonText: 'Да',
+          cancelButtonText: 'Отменить',
+          type: 'warning'
+        })
+        await this.$store.dispatch('post/remove', id)
+        this.posts = this.posts.filter(p => p._id !== id)
+
+        this.$message.success('Пост удален')
+      } catch (e) {}
     }
   }
 }
--- store/post.js
+++ store/post.js
@@ -6,7 +6,8 @@ export const actions = {
           {title: 'Post 1', date: new Date(), views: 22, comments: [1,2,3], _id: Math.random()},
           {title: 'Post 2', date: new Date(), views: 33, comments: [1,3], _id: Math.random()},
         ])
-      },2000)
+      },1000)
     })
-  }
+  },
+
 }
