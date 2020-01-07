7ab9f125cf6baa4f790f1d67379c3ff4a7ff5be0
Evgen904 <evgen904@gmail.com>
2020-01-07 23:17:52 +0300

    Редактирование поста

--- store/post.js
+++ store/post.js
@@ -1,13 +1,27 @@
+const posts = [
+  {title: 'Post 1', date: new Date(), views: 22, comments: [1,2,3], _id: 'id1'},
+  {title: 'Post 2', date: new Date(), views: 33, comments: [1,3], _id: 'id2'},
+];
+
 export const actions = {
   async fetchAdmin({}) {
     return await new Promise(resolve => {
       setTimeout(() => {
-        resolve([
-          {title: 'Post 1', date: new Date(), views: 22, comments: [1,2,3], _id: Math.random()},
-          {title: 'Post 2', date: new Date(), views: 33, comments: [1,3], _id: Math.random()},
-        ])
+        resolve(posts)
       },1000)
     })
   },
-
+  async remove({}) {
+    
+  },
+  async update({}, {id, text}) {
+    
+  },
+  async fetchAdminById({}, id) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve(posts.find(p => p._id === id))
+      },1000)
+    })
+  }
 }
