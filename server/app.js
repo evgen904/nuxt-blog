36f0f8d4c1d11d2133a21799d66aaf5c85a67b52
Evgen904 <evgen904@gmail.com>
2020-01-08 23:45:44 +0300

    Подключение MongoDB

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const keys = require('./keys')
 const app = express()
 
+mongoose.connect('mongodb://evgeniy:q123456@cluster0-shard-00-00-kmq61.mongodb.net:27017,cluster0-shard-00-01-kmq61.mongodb.net:27017,cluster0-shard-00-02-kmq61.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
+  .then(() => console.log('MongoDB connected...'))
+  .catch(error => console.log(error))
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
-
 
 // module.exports экспортируем переменную app на ружу
 module.exports = app
--- server/keys/keys.prod.js
+++ server/keys/keys.prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  
+  MONGO_URI: `mongodb+srv://evgeniy:q123456@cluster0-kmq61.mongodb.net/nuxt-blog`
 }
