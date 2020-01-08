cb8c4eb2d08a0c6bc3aea235484c3e6a0e0154f3
Evgen904 <evgen904@gmail.com>
2020-01-08 19:41:21 +0300

    Создание объекта конфигурации

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const keys = require('./keys')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(bodyParser.json())
 
 
 
