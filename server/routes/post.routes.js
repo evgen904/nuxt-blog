17a2e1b5a19ac6da6117e4800bf15a0c7e60f627
Evgen904 <evgen904@gmail.com>
2020-01-13 00:23:18 +0300

    Роуты постов

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,8 +1,58 @@
 // защита роутов, к примеру добавление, редактирование поста
 const passport = require('passport')
 
+// подкл. контроллер
+const ctr = require('../controllers/post.controller')
+
 // экспорт роута из экспресса
 const {Router} = require('express')
 const router = Router()
+
+// Admin, должны защищаться passport-ом
+// /api/post/admin
+
+// используем rest Api
+// post - создание (поста)
+router.post(
+  '/admin/',
+  // passport.authenticate - проверка наличия токена
+  passport.authenticate('jwt', {session: false}),
+  ctr.create
+)
+
+// get - получение (поста)
+router.get(
+  '/admin/',
+  passport.authenticate('jwt', {session: false}),
+  ctr.getAll
+)
+
+router.get(
+  '/admin/:id',
+  passport.authenticate('jwt', {session: false}),
+  ctr.getById
+)
+
+// put - изменение (поста)
+router.put(
+  '/admin/:id',
+  passport.authenticate('jwt', {session: false}),
+  ctr.update
+)
+
+// delete - удаление (поста)
+router.delete(
+  '/admin/:id',
+  passport.authenticate('jwt', {session: false}),
+  ctr.remove
+)
+
+// Base - публичные инпоинты
+// /api/post
+
+// ctr.getAll - подкл. методы
+router.get('/', ctr.getAll)
+router.get('/:id', ctr.getById)
+router.put('/:id', ctr.addView)
 
 module.exports = router
